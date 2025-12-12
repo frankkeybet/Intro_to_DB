a3ddd7bbe13a2cc0b03266d1e66ca3f0223b569b
Frankline <frankkeybet@gmail.com>
2025-12-12 13:15:46 +0300

    first commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -6,7 +6,7 @@ mydb=mysql.connector.connect(
 )
 mycursor=mydb.cursor()
 
-mycurser.execute(""" 
+mycursor.execute(""" 
 CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
