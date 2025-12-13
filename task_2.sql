a360de219bfa2ccd5b0fcc991f5ddef0397b1296
Frankline <frankkeybet@gmail.com>
2025-12-13 17:41:39 +0300

    created tables

--- task_2.sql
+++ task_2.sql
@@ -1,44 +1,68 @@
--- USE DATABASE
+
+-- Create the database if it doesn't already exist
+mycursor.execute("""
+CREATE DATABASE IF NOT EXISTS alx_book_store
+""")   
+
+
+-- Select the database to use
 USE alx_book_store;
 
--- AUTHORS TABLE
-CREATE TABLE IF NOT EXISTS authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+-- --------------------------------------------------------
+-- Table structure for Authors
+-- --------------------------------------------------------
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
-);
+)""")
 
--- BOOKS TABLE
-CREATE TABLE IF NOT EXISTS books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+-- --------------------------------------------------------
+-- Table structure for Books
+-- --------------------------------------------------------
+
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
-    author_id INT NOT NULL,
+    author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
-);
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+)""")
 
--- CUSTOMERS TABLE
-CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+-- --------------------------------------------------------
+-- Table structure for Customers
+-- --------------------------------------------------------
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
-);
+)""")
 
--- ORDERS TABLE
-CREATE TABLE IF NOT EXISTS orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT NOT NULL,
+-- --------------------------------------------------------
+-- Table structure for Orders
+-- --------------------------------------------------------
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-);
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+)""")
 
-
-CREATE TABLE IF NOT EXISTS order_details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT NOT NULL,
-    book_id INT NOT NULL,
+-- --------------------------------------------------------
+-- Table structure for Order_Details
+-- --------------------------------------------------------
+mycursor.execute("""
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
-);
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+)""")
+
